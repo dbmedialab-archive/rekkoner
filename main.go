30bcfd30a69d7afa3bcd3f5f95e384f71b15a91b
Eric Myhre <hash@exultant.us>
2017-12-08 14:17:54 +0100

    delete all the completely bonkers stuff
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- main.go
+++ main.go
@@ -4,16 +4,12 @@ import (
 	"flag"
 	"fmt"
 
-	"k8s.io/api/core/v1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/workqueue"
 )
 
 func main() {
@@ -73,67 +69,4 @@ func main() {
 			fmt.Printf(":: evt %T %#v\n\n", evt, evt)
 		}
 	}
-
-	return
-
-	// create the pod watcher
-	podListWatcher := cache.NewListWatchFromClient(clientset.CoreV1().RESTClient(), "namespaces", "", fields.Everything())
-
-	// create the workqueue
-	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
-
-	// Bind the workqueue to a cache with the help of an informer. This way we make sure that
-	// whenever the cache is updated, the pod key is added to the workqueue.
-	// Note that when we finally process the item from the workqueue, we might see a newer version
-	// of the Pod than the version which was responsible for triggering the update.
-	//
-	// There's a requirement that whatever you use as the type here implement
-	// k8s.io/apimachinery/pkg/runtime.Object ... so we do that.
-	indexer, informer := cache.NewIndexerInformer(podListWatcher, runtime.Object(nil), 0, cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			key, err := cache.MetaNamespaceKeyFunc(obj) // FIXME i have no idea why they don't use UUID here
-			if err == nil {
-				queue.Add(key)
-			}
-		},
-		UpdateFunc: func(old interface{}, new interface{}) {
-			key, err := cache.MetaNamespaceKeyFunc(new)
-			if err == nil {
-				queue.Add(key)
-			}
-		},
-		DeleteFunc: func(obj interface{}) {
-			// IndexerInformer uses a delta queue, therefore for deletes we have to use this
-			// key function.
-			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
-			if err == nil {
-				queue.Add(key)
-			}
-		},
-	}, cache.Indexers{})
-
-	_, _, _ = queue, indexer, informer
-
-	controller := NewController(queue, indexer, informer)
-
-	// We can now warm up the cache for initial synchronization.
-	// Let's suppose that we knew about a pod "mypod" on our last run, therefore add it to the cache.
-	// If this pod is not there anymore, the controller will be notified about the removal after the
-	// cache has synchronized.
-	if err := indexer.Add(&v1.Node{
-		ObjectMeta: meta_v1.ObjectMeta{
-			Name:      "hahaha",
-			Namespace: v1.NamespaceDefault,
-		},
-	}); err != nil {
-		panic(err)
-	}
-
-	// Now let's start the controller
-	stop := make(chan struct{})
-	defer close(stop)
-	go controller.Run(1, stop)
-
-	// Wait forever
-	select {}
 }
