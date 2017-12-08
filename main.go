9cab2091741c2724175264b1a99a5bde8faa0a6b
Eric Myhre <hash@exultant.us>
2017-12-08 14:08:35 +0100

    Replace that entire cacher monstrosity with plain watch.
    I have... no idea... why that cacher thing is 1/80th as complicated as it is.
    
    As described in the previous commit, though, we're still going to try to
    go *even simpler* than this: it is actively preferable *not* to map things
    into the type system, both because it may lose fields if we don't have
    a perfect version match, and because we'd quite simply end up using *more
    reflection* in order to iterate things again, and that's *going backwards*.
    
    Unfortunately, the k8s API client libraries are *not* making that easy on us.
    More research todo.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"k8s.io/api/core/v1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/workqueue"
@@ -34,6 +37,44 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	// Okay, trying to figure out what this cacher thing does and I ended up having
+	// to read a `NewNamedReflector` factory and *still* haven't found any meat...
+	// fuck this.  This is the worst architecture astronauting I've ever seen.
+	// I literally can't find any logic that *does* *any* *thing*.
+
+	watchFunc := func(c rest.Interface, resource string, namespace string, fieldSelector fields.Selector) (watch.Interface, error) {
+		options := meta_v1.ListOptions{}
+		options.Watch = true
+		options.FieldSelector = fieldSelector.String()
+		return c.Get().
+			Namespace(namespace).
+			Resource(resource).
+			VersionedParams(&options, meta_v1.ParameterCodec).
+			Watch() // I think I'm actually pretty ok with using their API up to right about here.
+		// nope it still already did the hypermagic typed deserialize
+		// I really don't want that
+		// it is so fucking much easier to iterate over *maps* to do this job
+		// i'm literally going to back ALLLLL the way up off to doing my own rest client.
+	}
+	watchNS, err := watchFunc(clientset.CoreV1().RESTClient(), "namespaces", "", fields.Everything())
+	if err != nil {
+		panic(err)
+	}
+	watchPods, err := watchFunc(clientset.CoreV1().RESTClient(), "pods", "", fields.Everything())
+	if err != nil {
+		panic(err)
+	}
+	for {
+		select {
+		case evt := <-watchNS.ResultChan():
+			fmt.Printf(":: evt %T %#v\n\n", evt, evt)
+		case evt := <-watchPods.ResultChan():
+			fmt.Printf(":: evt %T %#v\n\n", evt, evt)
+		}
+	}
+
+	return
 
 	// create the pod watcher
 	podListWatcher := cache.NewListWatchFromClient(clientset.CoreV1().RESTClient(), "namespaces", "", fields.Everything())
