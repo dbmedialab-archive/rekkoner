58f679051631c467bf11f335811a4cdeea8133e9
Eric Myhre <hash@exultant.us>
2018-01-08 14:14:00 +0100

    Research API auto-exploration.  Appears to be an un-indexed operation requiring O(n) http requests just to get started getting started.  Very nice.
    </sarcasm>
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- main.go
+++ main.go
@@ -33,6 +33,34 @@ func main() {
 		panic(err)
 	}
 
+	// Figuring out groups, versions, etc:
+	//
+	// I'd kind of like to *not* have to explicitly specify e.g. "extensions/v1beta1/deployments"
+	// or similar pattern of string for *every single* resource I need *multiplied* by wherever
+	// those versions are on the particular version of k8s master I'm talking to.
+	//
+	// So how do we auto-detect that?
+	//
+	// - get 'servergroups.json'
+	// - iterate over '.groups' list
+	// - take '.name' and '.preferredVersion.version' -- plug them into your `schema.GroupVersion`, woot
+	//   - n.b. I *think* strcat'ing those is identical to '.preferredVersion.groupVersion' but ¯\_(ツ)_/¯
+	// - ok, but *what* is that the group+version tuple **for**???  well we don't know yet
+	// - get '$group/$version/serverresources.json'... for all group+version tuples I guess
+	//   - there's apparently no index that lets us find out which kinds will be where... so, yep, all
+	//   - so yes, you need to *do $n$ https requests* to *find out what you can ask for*, nbd
+	//     - this explains why `~/.kube/cache/discovery/*` probably exists in your homedir... -.-
+	//       - btw note well none of these have generation numbers or anything you could cachebust with either
+	// - iterate through this and look for '.name'
+	//   - THIS you can finally match on!  look for e.g. "deployments" here and you should find it.
+	// - and you may want to look for the '.namespaced' bool here as well (but your business
+	//   logic probably already know that one, in practice).
+	//
+	// Implementation: ugh, todo.
+	// (SURELY this should exist in the k8s client packages already.  But can I get at
+	// it in a way that I can A: cache more sanely, and B: compose with the `dynamic.NewClient` route
+	// so that it's literally even fit for use?  Based on prior experiences: less than 50% chance.)
+
 	// Please
 	config.GroupVersion = &schema.GroupVersion{Version: "v1"}
 	config.APIPath = "/api"
@@ -47,7 +75,7 @@ func main() {
 	printyeUnstructuredList("namespaces", obj.(*unstructured.UnstructuredList), os.Stdout)
 
 	config.APIPath = "/apis"
-	config.GroupVersion = &schema.GroupVersion{Version: "extensions/v1beta1"}
+	config.GroupVersion = &schema.GroupVersion{Group: "extensions", Version: "v1beta1"}
 	dyn, err = dynamic.NewClient(config)
 	if err != nil {
 		panic(err)
