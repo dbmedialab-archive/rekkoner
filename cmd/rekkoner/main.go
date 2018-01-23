e1305f28da0968d228b218692094daf0350a9f03
Eric Myhre <hash@exultant.us>
2018-01-23 14:41:17 +0100

    Add new command which parses files.
    Testing against files in the wild, mea cupla
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ cmd/rekkoner/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	"github.com/dbmedialab/rekkoner/velcro/k8s"
+)
+
+func main() {
+	allObjs := []unstructured.Unstructured{}
+	walkFunc := func(path string, info os.FileInfo, err error) error {
+		// Only interested in plain files.
+		if !info.Mode().IsRegular() {
+			return nil
+		}
+		// Open file and parse.
+		// The k8s parser will error for things like "Object 'Kind' is missing",
+		//  so we're fairlyyyy sure we've got sane k8s objects if no error.
+		f, err := os.OpenFile(path, os.O_RDONLY, 0)
+		if err != nil {
+			log.Printf("error: %q: %s\n", path, err)
+		}
+		objs, err := k8s.DecodeObjects(f)
+		if err != nil {
+			log.Printf("error: %q: %s\n", path, err)
+		}
+		allObjs = append(allObjs, objs...)
+		return nil
+	}
+	if err := filepath.Walk(os.Args[1], walkFunc); err != nil {
+		log.Fatalf("error: %s\n", err)
+	}
+	kindCount := map[string]int{}
+	for _, obj := range allObjs {
+		kind := obj.Object["kind"].(string)
+		kindCount[kind]++
+	}
+	fmt.Printf("%s\n", kindCount)
+}
